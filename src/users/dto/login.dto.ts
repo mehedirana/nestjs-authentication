11b4e149a95da8f984c1115389ddbc42771a4d07
mehedirana <mdmehedirana@gmail.com>
2026-01-08 12:29:29 +0600

    details

--- src/users/dto/login.dto.ts
+++ src/users/dto/login.dto.ts
@@ -1,13 +1,13 @@
 // src/auth/dto/login.dto.ts
 import { IsEmail, IsString, MinLength, MaxLength } from 'class-validator';
-import { ApiProperty } from '@nestjs/swagger';
+// import { ApiProperty } from '@nestjs/swagger';
 
 export class LoginDto {
-  @ApiProperty({ example: 'user@example.com' })
+  // @ApiProperty({ example: 'user@example.com' })
   @IsEmail()
   email: string;
 
-  @ApiProperty({ example: 'Password123!' })
+  // @ApiProperty({ example: 'Password123!' })
   @IsString()
   @MinLength(6)
   @MaxLength(100)
